8a4ba0183a5d88ef74ecc1933129a1075eeb2c4c
Joshua Clayton <jclayton@thoughtbot.com>
2012-03-27 12:11:59 -0400

    Make sure 15 tweets show up in the results

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -5,7 +5,11 @@ When /^I search for the hash tag "([^"]*)"$/ do |hashtag|
 end
 
 Then /^I should see tweets containing "([^"]*)"$/ do |tweet_text|
+  wait_until { all("ul.tweets li").count == 15 }
+
+  search_term = /#{tweet_text}/i
+
   within "ul.tweets" do
-    page.should have_css("li", text: tweet_text)
+    page.should have_css("li", text: search_term, count: 15)
   end
 end
